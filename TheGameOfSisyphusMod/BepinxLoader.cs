c0f8b723c17e84bc75b3d7bb337c0ba1e346309e
Justin Wittwer <justin.wittwer@gmx.de>
2024-07-22 18:16:36 +0200

    initial commit

---
+++ TheGameOfSisyphusMod/BepinxLoader.cs
@@ -0,0 +1,28 @@
+﻿using BepInEx;
+using HarmonyLib;
+using TheGameOfSisyphusMod.patches;
+using UnityEngine;
+
+namespace TheGameOfSisyphusMod
+{
+    [BepInPlugin("com.justin8303.thegameofsisyphusmod", "TheGameOfSisyphusMod", "1.0.0")]
+    public class BepinxLoader : BaseUnityPlugin
+    {
+        public const string
+            MODNAME = "TGOSMod",
+            AUTHOR = "Justin8303",
+            GUID = AUTHOR + "." + MODNAME,
+            VERSION = "1.0.0";
+
+        public static Harmony HarmonyInstance;
+        
+        public void Awake()
+        {
+            Logger.LogInfo($"Loaded {MODNAME} v{VERSION} by {AUTHOR}");
+            
+            HarmonyInstance = new Harmony(GUID);
+            HarmonyInstance.PatchAll(typeof(GameManagerPatches));
+            Logger.LogInfo("Patched GameManager.GenerateBall");
+        }
+    }
+}
---
+++ TheGameOfSisyphusMod/patches/GameManagerPatches.cs
@@ -0,0 +1,47 @@
+﻿using System.Threading;
+using BepInEx.Logging;
+using HarmonyLib;
+using Unity.VisualScripting;
+using UnityEngine;
+using Logger = BepInEx.Logging.Logger;
+
+namespace TheGameOfSisyphusMod.patches
+{
+    [HarmonyPatch(typeof(GameManager))]
+    public class GameManagerPatches
+    {
+        private static GameObject _rock;
+        
+        [HarmonyPatch("GenerateBall")]
+        [HarmonyPostfix]
+        public static void GenerateBall_postfix()
+        {
+            //log the ball generation
+            ManualLogSource logger = new ManualLogSource("TheGameOfSisyphusMod");
+            Logger.Sources.Add(logger);
+            
+            logger.LogInfo("Ball generated");
+            
+            //find Rock(Clone) and log its position
+            GameObject rock = GameObject.Find("Rock(Clone)");
+            if (rock != null && !_rock)
+            {
+                _rock = rock;
+            }
+        }
+        
+        [HarmonyPatch("Update")]
+        [HarmonyPostfix]
+        public static void Update_postfix()
+        {
+            //log the update
+            ManualLogSource logger = new ManualLogSource("TheGameOfSisyphusMod");
+            Logger.Sources.Add(logger);
+
+            if (_rock)
+            {
+                logger.LogInfo($"Rock position: {_rock.transform.position}");
+            }
+        }
+    }
+}

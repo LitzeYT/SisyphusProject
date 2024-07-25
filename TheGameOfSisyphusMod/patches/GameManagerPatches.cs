b7b9b8b2e55a6923e2986b04cd5cb24f158971f4
Simon Tepel <tepel.simon@gmail.com>
2024-07-25 21:54:54 +0200

    Update GameManagerPatches.cs

--- TheGameOfSisyphusMod/patches/GameManagerPatches.cs
+++ TheGameOfSisyphusMod/patches/GameManagerPatches.cs
@@ -7,6 +7,7 @@ using HarmonyLib;
 using UnityEngine;
 using UnityEngine.Networking;
 
+
 namespace TheGameOfSisyphusMod.patches
 {
     [HarmonyPatch(typeof(GameManager))]
@@ -16,6 +17,7 @@ namespace TheGameOfSisyphusMod.patches
         private const string Url = "https://hobauflock.cloud/api/v1/User/";
         private const float Interval = 5f; // Interval in seconds
         private static ManualLogSource _logger;
+        private static string UserId = "";
 
         static GameManagerPatches()
         {
@@ -36,16 +38,54 @@ namespace TheGameOfSisyphusMod.patches
 
             _logger.LogInfo("EmeraldRock: " + _emeraldRock);
 
+            string path = Application.dataPath + "/ModdingConfig/ModdingConfig.json";
+            if (System.IO.File.Exists(path))
+            {
+                string[] lines = System.IO.File.ReadAllLines(path);
+                UserId = lines[0];   
+
+                _logger.LogInfo("Loaded UserId: " + UserId);
+                //load texture form ModdingConfig folder "Rock_emerald_AlbedoTransparency.png" and apply it to the rock material
+                var texture = new Texture2D(2, 2, TextureFormat.ARGB32, false);                
+                texture.LoadImage(System.IO.File.ReadAllBytes(Application.dataPath + "/ModdingConfig/Rock_emerald_AlbedoTransparency.png"));                
+                texture.wrapMode = TextureWrapMode.Clamp;
+                var material = _emeraldRock.GetComponent<MeshRenderer>().material;                
+                material.mainTexture = texture;
+
+                //load Rock_emerald_MetallicSmoothness.png and apply it to the rock materials metallic and smoothness
+                var texture2 = new Texture2D(2, 2, TextureFormat.ARGB32, false);
+                texture2.LoadImage(System.IO.File.ReadAllBytes(Application.dataPath + "/ModdingConfig/Rock_emerald_MetallicSmoothness.png"));
+                texture2.wrapMode = TextureWrapMode.Clamp;
+                material.SetTexture("_MetallicGlossMap", texture2);
+                
+            }
+            else
+            {
+                _logger.LogError("ModdingConfig.json not found");               
+                System.IO.Directory.CreateDirectory(Application.dataPath + "/ModdingConfig");
+                System.IO.File.WriteAllText(path, "Replace_this_with_your_user_id_no_quotes_no_spaces_atleast_3_characters");   
+
+                //quit the game
+                Application.Quit();            
+            }
+
+
+            _logger.LogInfo("UserId: " + UserId);
+
+
+
             if (_emeraldRock != null)
             {
-                MonoBehaviourSingleton.Instance.StartCoroutine(SendRockPositionToServer("TestSisyphus", _emeraldRock));
+                MonoBehaviourSingleton.Instance.StartCoroutine(SendRockPositionToServer(UserId, _emeraldRock));
             }
+
+
         }
 
         private static IEnumerator SendRockPositionToServer(string userId, GameObject stone)
         {
             while (true)
-            {                
+            {
                 _logger.LogInfo("Sending data to server");
 
                 int height = (int)(stone.transform.position.y * 100);
@@ -53,7 +93,7 @@ namespace TheGameOfSisyphusMod.patches
                 _logger.LogInfo("URL: " + ls);
                 using (UnityWebRequest request = new UnityWebRequest(ls, "POST"))
                 {
-                    
+
                     request.downloadHandler = new DownloadHandlerBuffer();
 
                     yield return request.SendWebRequest();
